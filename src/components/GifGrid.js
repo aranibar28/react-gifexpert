7f33a4fa41794935fa54f03d0611f475114ab110
aranibar <aranibargerson28@gmail.com>
2021-11-16 00:05:33 -0500

    agregando efectos de estilo

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -7,7 +7,7 @@ export const GifGrid = ({ category }) => {
 
   return (
     <>
-      <h3>{category}</h3>
+      <h3 className="animate__animated animate__flash">{category}</h3>
       {loading && <p>Loading...</p>}
       <div className="card-grid">
         {images.map((img) => (
