581c6030bd7b8d38c6582adb7ad84880ed414b7c
Gerson Aranibar <aranibargerson28@gmail.com>
2022-11-19 09:39:35 -0500

    Update getGif.js

--- src/components/helpers/getGif.js
+++ src/components/helpers/getGif.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=s21dwz9cakBM6Tfj0nrYVu9vqlA2t9IA`;
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=`;
   const resp = await fetch(url);
   const { data } = await resp.json();
 
